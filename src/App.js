5b8d977f2e4096ae478d94c8ee1585d56ec475bd
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-10-02 21:43:51 -0400

    Calcular precio con impuestos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+function impuestoEstado(estado) {
+  let impuestos = { 
+    "UT":0.0665,
+    "NV":0.08,
+    "TX":0.0625,
+    "AL":0.04,
+    "CA":0.0825,
+    "": 0 };
+  return impuestos[estado];
+}
+
 function calcularTotal(cantidad, precio, estado) {
   let subTotal = cantidad * precio;
+  let impuesto = impuestoEstado(estado);
+  subTotal = subTotal + subTotal * impuesto;
   return subTotal;
 }
 export default calcularTotal;
--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,24 @@ describe("Totalizador ", () => {
   it("deberia calcular para un cantidad", () => {
     expect(calcularTotal(2, 2, "")).toEqual(4);
   });
+  it("deberia calcular para un estado por defecto", () => {
+    expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
+  });
+  it("deberia calcular para un estado CA", () => {
+    expect(calcularTotal(3, 2, "CA")).toEqual(6.495);
+  });
+  it("deberia calcular para un estado UT", () => {
+    expect(calcularTotal(3, 2, "UT")).toEqual(6.399);
+  });
+  it("deberia calcular para un estado NV", () => {
+    expect(calcularTotal(3, 2, "NV")).toEqual(6.48);
+  });
+  it("deberia calcular para un estado TX", () => {
+    expect(calcularTotal(3, 2, "TX")).toEqual(6.375);
+  });
+  it("deberia calcular para un estado AL", () => {
+    expect(calcularTotal(3, 2, "AL")).toEqual(6.24);
+  });
   
 });
 
